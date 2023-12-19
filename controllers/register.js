6cb35972a1c39d99ca7477e70fbdc08189e60f54
Marc Edison Estaca <mebestaca@gmail.com>
2023-12-18 22:55:44 -0700

    implemented register controller

---
+++ controllers/register.js
@@ -0,0 +1,31 @@
+const handleRegister = (req, res, database, bcrypt) => {
+    const { email, name, password } = req.body;
+    const hash = bcrypt.hashSync(password, 10);
+    database.transaction(trx =>{
+        trx.insert({
+            hash: hash,
+            email: email,
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return database('users')
+                .returning('*')
+                .insert({
+                    name: name,
+                    email: email,
+                    joined: new Date()
+                })
+                .then(user => res.json(user[0]))
+                .catch(err => res.status(400).json('unable to register'));
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
+    });
+
+    
+}
+
+module.exports = {
+    handleRegister: handleRegister
+};
