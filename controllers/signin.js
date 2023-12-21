53eb1e4770c2c23c7f9161eb587ca544fb502379
Marc Edison Estaca <mebestaca@gmail.com>
2023-12-20 23:12:37 -0700

    added some validation on signin js

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,21 +1,26 @@
 const handleSignIn = (req, res, database, bcrypt) => {
+    const { email, password } = req.body;
+    if (!email || !password || !email.includes('@') || password.length < 6) {
+        return res.status(400).json("Incorrect form submission");
+    }
+
     database.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
+        .where('email', '=', email)
         .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            const isValid = bcrypt.compareSync(password, data[0].hash);
             if (isValid){
                 return database.select('*').from('users')
-                    .where('email', '=', req.body.email)
+                    .where('email', '=', email)
                     .then(user => {
                         res.json(user[0])
                     })
-                    .catch(err => res.status(400).json('unable to retrieve user credentials'));
+                    .catch(err => res.status(400).json('Unable to retrieve user credentials'));
             }
             else {
-                res.status(400).json('wrong credentials');
+                res.status(400).json('Wrong credentials');
             }
         })
-        .catch(err => res.status(400).json('wrong credentials'));
+        .catch(err => res.status(400).json('Wrong credentials'));
 };
 
 module.exports = {
