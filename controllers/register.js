cdfb11997ecb25a02f8ea5ba552246902fbc6df2
Marc Edison Estaca <mebestaca@gmail.com>
2023-12-20 23:10:37 -0700

    added some validation on register js

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,8 @@
 const handleRegister = (req, res, database, bcrypt) => {
     const { email, name, password } = req.body;
+    if (!email || !name || !password || name.length < 6 || !email.includes('@') || password.length < 6) {
+        return res.status(400).json("Incorrect form submission");
+    }
     const hash = bcrypt.hashSync(password, 10);
     database.transaction(trx =>{
         trx.insert({
@@ -17,7 +20,7 @@ const handleRegister = (req, res, database, bcrypt) => {
                     joined: new Date()
                 })
                 .then(user => res.json(user[0]))
-                .catch(err => res.status(400).json('unable to register'));
+                .catch(err => res.status(400).json('Unable to register'));
         })
         .then(trx.commit)
         .catch(trx.rollback)
