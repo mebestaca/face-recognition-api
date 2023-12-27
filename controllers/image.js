e8845edc7498676c873d58ff59a4aa4f53118616
Marc Edison Estaca <mebestaca@gmail.com>
2023-12-26 22:06:04 -0700

    added imagehandler controller

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,60 @@
+const handleImageDetect = (req, res) => {
+    const { imageUrl } = req.body;
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+    // In this section, we set the user authentication, user and app ID, model details, and the URL
+    // of the image we want as an input. Change these strings to run your own example.
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = '9a1c86d08e8841ebae2d7571f88f058b';
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = 'mebestaca';       
+    const APP_ID = 'face-detection';
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'face-detection';
+    const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';  
+    const IMAGE_URL = imageUrl;
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+    ///////////////////////////////////////////////////////////////////////////////////
+
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+    // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+    // this will default to the latest version_id
+
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+        .then(response => response.json())
+        .then(result => res.json(result))
+        .catch(error => console.log('error', error));
+}
+
 const handleImage = (req, res, database) => {
     const { id } = req.body;
     database('users').where('id', '=', id)
@@ -8,5 +65,6 @@ const handleImage = (req, res, database) => {
 };
 
 module.exports = {
-    handleImage
+    handleImage,
+    handleImageDetect,
 }
