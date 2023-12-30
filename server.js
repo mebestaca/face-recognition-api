13425ef553a48a41980acb2e5674b1ee0f933872
Marc Edison Estaca <mebestaca@gmail.com>
2023-12-30 04:35:17 -0700

    implemented node-fetch

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 const bcrypt = require('bcryptjs');
 const knex = require('knex');
-const port = 3000;
+const nodeFetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -29,8 +29,8 @@ app.post('/signin', (req, res) => { signin.handleSignIn(req, res, database, bcry
 app.post('/register', (req, res) => { register.handleRegister(req, res, database, bcrypt) });
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, database) });
 app.put('/image', (req, res) => { image.handleImage(req, res, database) } );
-app.post('/imagedetect', (req, res) => { image.handleImageDetect(req, res) } );
+app.post('/imagedetect', (req, res) => { image.handleImageDetect(req, res, nodeFetch) } );
 
-app.listen(port, () => {
-    console.log(`Listening to port ${port}`);
+app.listen(() => {
+    console.log(`Server is live...`);
 });
